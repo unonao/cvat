830d319b7b63d8af360585a15f1aacd12eb1b074
Gen Takagi <hamham37337@gmail.com>
2021-03-07 18:06:47 +0900

    authfication change

--- cvat/apps/authentication/auth_basic.py
+++ cvat/apps/authentication/auth_basic.py
@@ -23,10 +23,6 @@ def create_user(sender, instance, created, **kwargs):
         db_group = Group.objects.get(name=AUTH_ROLE.ANNOTATOR)
         instance.groups.add(db_group)
 
-        db_group = Group.objects.get(name=AUTH_ROLE.OBSERVER)
-        instance.groups.add(db_group)
-
-
     for group_name in settings.DJANGO_AUTH_DEFAULT_GROUPS:
         db_group = Group.objects.get(name=getattr(AUTH_ROLE, group_name))
         instance.groups.add(db_group)
